da3f91ba473cfc236fe3d0f47e9df89fea05d3ed
apalm112 <apalm112@gmail.com>
2016-12-10 21:53:49 -0800

    printQuote function almost completed.

--- js/script.js
+++ js/script.js
@@ -1,35 +1,49 @@
 // Random Quote Generator for Treehouse Project #1
 var getQuote;
+var getRQ;
+
 var quotesArray = [];  // array for Stretch Goal of not repeating quotes until all have been displayed
 
 
 function getRandomQuote() {
     // selects random quote from quotes array
-    //for (var idx=0;idx<quotes.length;idx++) {}
+    //for (var idx=0;idx<quotes.length;idx++) {} loop for stretch goal
     var rando = Math.floor(Math.random() * quotes.length);
-    console.log(rando);
+    // console.log(rando);
     quotesArray.push( quotes[rando].quote );
-    console.log(quotes[rando].quote);
+    // console.log(quotes[rando].quote);
     getQuote = quotes[rando].quote;
     return getQuote;
-
 }
-getRandomQuote();
 
 function printQuote() {
     var getRQ = getRandomQuote();
+    // for in loop searches through the objects to find matching quote & then grab that objects corresponding properties for the template
+        for (var key in quotes){
+            if (quotes[key].quote === getQuote){
+                var source = quotes[key].source;
+                var citation = quotes[key].citation;
+                var year = quotes[key].year;
+            }
+        }
+    template = '<p class="quote">' + getRQ + '</p>';
+    template += '<p class="source">' + source;
     // constructs a string using this template:
-    //     <p class="quote">quote here</p>
-    //     <p class="source">source here
-    //         <span class="citation">citation here</span>
-    //         <span class="year">year here</span>
-    //     </p>
     // printQuote does not add for a missing citation or year property
+    template += '<span class="citation">' + citation + '</span>';
+    template += '<span class="year">' + year + '</span></p>';
+
     // printQuote puts final HTML string to the page using:
-    //     document.getElementById('quote-box').innerHTML
+    var getQuoteBox = document.getElementById('quote-box');
+    getQuoteBox.innerHTML = template;
 }
 
+function checkForRepeats() {
+    // stretch goal function to not repeat quotes until all quotes have been shown, use console.log();
+}
 
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
+// printQuote();
