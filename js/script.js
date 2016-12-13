9a9dd26aa8b74aca4b15ff0226259e0ea6505e50
apalm112 <apalm112@gmail.com>
2016-12-12 17:22:38 -0800

    Changed function to change background color only when the quote changes.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 // Random Quote Generator for Treehouse Project #1
 // Global variables
-var getQuote;
+var newQuote;
 var getRQ;
 var source;
 var citation;
 var year;
-var getCurrentQuote;
-var counter =0;
+var previousQuote;
+var counter = 0;
+var picardCounter = 0;
 var repeatCounter = 0;
 
 var quotesShownArray = [];  // array for Stretch Goal of not repeating quotes until all have been displayed
@@ -15,23 +16,32 @@ function getRandomQuote() {
     // creates a random number ranging from 1 to the length of the quotes array
     var rando = Math.floor(Math.random() * quotes.length);
     // selects random quote from quotes array
-    getQuote = quotes[rando].quote;
-    //checkForRepeats();
-    return getQuote;
+    newQuote = quotes[rando].quote;
+
+    while (repeatCounter) {
+        if (newQuote === previousQuote) {
+            rando = Math.floor(Math.random() * quotes.length);
+            newQuote = quotes[rando].quote;
+        } else {
+            break;
+        }
+    }
+    repeatCounter = 1;
+    return newQuote;
 }
+
 function getObjectProps() {
     /*for in loop searches through the objects to find matching
     quote & then grabs that objects properties
     for the template*/
     for (var key in quotes){
-        if (quotes[key].quote === getQuote){
+        if (quotes[key].quote === newQuote){
             source = quotes[key].source;
             citation = quotes[key].citation;
             year = quotes[key].year;
         }
     }
 }
-
 
 function printQuote() {
     var getRQ = getRandomQuote();
@@ -61,56 +71,59 @@ function changeBackgroundColor() {
     green = Math.floor(Math.random() * 256+1);
     var color = 'rgba(' + red + ',' + green + ',' + blue + ',' + 0.9 + ')';
     var getContainer = document.getElementById('bgColor');
-    getCurrentQuote = quotesShownArray[quotesShownArray.length-2];
-    if (getQuote !== getCurrentQuote) {
+    quotesShownArray.push( newQuote );
+    // sets variable to the previous shown quote
+    previousQuote = quotesShownArray[quotesShownArray.length-2];
+    //checks if new quote is different than the previous quote
+    if (newQuote !== previousQuote) {
     getContainer.style.backgroundColor = color;
     console.log(color + '  ' + source);
+    console.log(newQuote + '    ' + previousQuote);
     }
 }
 
 function picard() {
     //This function adds/removes a jpg to the page for the Picard quote.
     // if conditional to addd picard jpg
-    if (getQuote === quotes[2].quote && counter === 0) {
+    if (newQuote === quotes[2].quote && picardCounter === 0) {
         var getPicard = document.getElementById('picard');
         var pic = document.createElement('img');
         pic.setAttribute('src', 'img/hrus_ex_picards_4_lights_dd.jpg');
         getPicard.appendChild(pic);
-        counter = 1;
+        picardCounter = 1;
     }
     // if conditional to remove picard jpg when quote changes
-    if (getQuote != quotes[2].quote && counter === 1) {
+    if (newQuote != quotes[2].quote && picardCounter === 1) {
         var getPicard = document.getElementById('picard');
         getPicard.firstChild.remove();
-        counter = 0;
+        picardCounter = 0;
     }
 }
 
 function checkForRepeats() {
     //TODO: stretch goal function to not repeat quotes until all quotes have been shown, use console.log();
     if (repeatCounter) {
-            if (quotesShownArray.includes(getQuote)) {
+            if (quotesShownArray.includes(newQuote)) {
                 // get new quote
-                console.log('before: ' + getQuote);
+                console.log('before: ' + newQuote);
                 getRandomQuote();
                 console.log('Wɘ Aᴙɘ ɘxqɘᴙiɘᴎↄiᴎg TɘↄHᴎiↄAl biꟻꟻiↄUlTiɘ﻿');
             } else {
                 // adds the selected quote to an array
-                quotesShownArray.push( getQuote );
+                quotesShownArray.push( newQuote );
                 console.log('ELSE CLAUSE HERE TO PUSH TO ARRAY');
             }
     }
     repeatCounter += 1;
-    getCurrentQuote = quotesShownArray[quotesShownArray.length-2];
-    console.log('Previous Quote: ' + getCurrentQuote);
-    console.log('New Quote: ' + getQuote);
+    previousQuote = quotesShownArray[quotesShownArray.length-2];
+    console.log('Previous Quote: ' + previousQuote);
+    console.log('New Quote: ' + newQuote);
 }
 
 // This do while loop runs once to load a random quote when the page is first loaded & push that value to an array
 do {
     printQuote();
-    // adds the first selected quote to an array
-    quotesShownArray.push( getQuote );
+    console.log('Do While Loop, execute once only!');
 } while (false);
 
 // event listener to respond to "Show another quote" button clicks
