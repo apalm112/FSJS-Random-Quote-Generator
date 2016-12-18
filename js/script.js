71a94ae1b320cb0aad15d415ec8f3f0afed79654
apalm112 <apalm112@gmail.com>
2016-12-18 00:54:23 -0800

    Replaced addEventListener w/ .on() method call to load quotes.

--- js/script.js
+++ js/script.js
@@ -119,6 +119,5 @@ $(document).ready(function(){
     printQuote();
 });
 
-// event listener to respond to "Show another quote" button clicks
-// when user clicks anywhere on the button, the "printQuote" function is called
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+// when user clicks on the button, the "printQuote" function is called
+$('#loadQuote').on('click', printQuote);
